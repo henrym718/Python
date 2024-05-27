c847ebd25bc5d7422ace91f20f5233d21efd6e49
Henrym.718 <henrym.718@hotmail.com>
2024-05-27 11:22:29 +0200

    update

---
+++ ex02/numeric-data.py
@@ -0,0 +1,25 @@
+myValue = 2
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)) )
+
+
+myValue = 3.14
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)) )
+
+myValue = 5.j
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)) )
+
+myValue = True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)) )
+
+myValue = False
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)) )
