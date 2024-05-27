7cd52479b7356d0a6296905613b442a30257affe
Henrym.718 <henrym.718@hotmail.com>
2024-05-27 11:44:28 +0200

    ex04

---
+++ ex04/collections.py
@@ -0,0 +1,39 @@
+#print(myFruitList[0])
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+
+#Acceso a una lista por posición
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+
+#Modificación de los valores de una lista
+myFruitList[2] = "orange"
+print(myFruitList)
+
+#Tipo de dato de tupla
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+
+
+#Acceso a una tupla por posición
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+
+#Tipo de dato de diccionario
+myFavoriteFruitDictionary = {
+  "Akua" : "apple",
+  "Saanvi" : "banana",
+  "Paulo" : "pineapple"
+}
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+
+#Acceso al diccionario por nombre
+print(myFavoriteFruitDictionary["Akua"])
+print(myFavoriteFruitDictionary["Saanvi"])
+print(myFavoriteFruitDictionary["Paulo"])
